02e12ef7bd99e6594d67410eb751d9e6e894e4c0
Oren Papa <oren.papa@cardoai.com>
2026-08-13 09:16:39 +0200

    Fix js loading on mobile

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/standard_financial_tools/',
   build: {
     rollupOptions: {
       input: {
